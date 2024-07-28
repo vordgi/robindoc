f2a393cd85dcce31235189bcb799715709d08435
Vordgi <sasha2822222@gmail.com>
2024-07-28 15:04:13 +0400

    rd-34 support local md pathname

--- packages/robindoc/src/generate-metadata-factory.ts
+++ packages/robindoc/src/generate-metadata-factory.ts
@@ -1,9 +1,20 @@
 import { Marked } from "marked";
 import { set } from "dot-prop";
+import { readFile } from "fs/promises";
 
-export const generateMetadataFactory = (content: string) => {
-    const generateMetadata = () => {
-        const tree = new Marked({ async: true }).lexer(content);
+type GenerateMetadataFactoryOpts = { content: string; uri?: undefined } | { uri: string; content?: undefined };
+
+export const generateMetadataFactory = (opts: GenerateMetadataFactoryOpts) => {
+    const { content, uri } = opts;
+
+    const generateMetadata = async () => {
+        const data = uri ? await readFile(uri, "utf-8") : content;
+
+        if (!data) {
+            throw new Error("Robindoc: Please provide content or valid uri");
+        }
+
+        const tree = new Marked({ async: true }).lexer(data);
 
         const metaData = Object.entries(tree.links).reduce<Record<string, string>>((acc, cur) => {
             const [key, value] = cur;
