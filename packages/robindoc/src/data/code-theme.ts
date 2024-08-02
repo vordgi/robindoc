aa48fdf8ad27edf0376a929caccf8e488b6c6f45
Vordgi <sasha2822222@gmail.com>
2024-08-02 20:16:23 +0400

    rd-11 style code block

---
+++ packages/robindoc/src/data/code-theme.ts
@@ -0,0 +1,496 @@
+import { type ThemeRegistrationAny } from "shiki";
+
+export const githubDynamic: ThemeRegistrationAny = {
+    colors: {
+        "activityBar.activeBorder": "var(--r-cl-1)",
+        "activityBar.background": "var(--r-cl-2)",
+        "activityBar.border": "var(--r-cl-3)",
+        "activityBar.foreground": "var(--r-cl-4)",
+        "activityBar.inactiveForeground": "var(--r-cl-5)",
+        "activityBarBadge.background": "var(--r-cl-6)",
+        "activityBarBadge.foreground": "var(--r-cl-2)",
+        "badge.background": "var(--r-cl-7)",
+        "badge.foreground": "var(--r-cl-8)",
+        "breadcrumb.activeSelectionForeground": "var(--r-cl-9)",
+        "breadcrumb.focusForeground": "var(--r-cl-4)",
+        "breadcrumb.foreground": "var(--r-cl-10)",
+        "breadcrumbPicker.background": "var(--r-cl-11)",
+        "button.background": "var(--r-cl-12)",
+        "button.foreground": "var(--r-cl-2)",
+        "button.hoverBackground": "var(--r-cl-13)",
+        "button.secondaryBackground": "var(--r-cl-3)",
+        "button.secondaryForeground": "var(--r-cl-14)",
+        "button.secondaryHoverBackground": "var(--r-cl-15)",
+        "checkbox.background": "var(--r-cl-11)",
+        "checkbox.border": "var(--r-cl-15)",
+        "debugToolBar.background": "var(--r-cl-2)",
+        descriptionForeground: "var(--r-cl-10)",
+        "diffEditor.insertedTextBackground": "var(--r-cl-16)",
+        "diffEditor.removedTextBackground": "var(--r-cl-17)",
+        "dropdown.background": "var(--r-cl-11)",
+        "dropdown.border": "var(--r-cl-3)",
+        "dropdown.foreground": "var(--r-cl-4)",
+        "dropdown.listBackground": "var(--r-cl-2)",
+        "editor.background": "var(--r-cl-2)",
+        "editor.findMatchBackground": "var(--r-cl-18)",
+        "editor.findMatchHighlightBackground": "var(--r-cl-19)",
+        "editor.focusedStackFrameHighlightBackground": "var(--r-cl-20)",
+        "editor.foldBackground": "var(--r-cl-21)",
+        "editor.foreground": "var(--r-cl-22)",
+        "editor.inactiveSelectionBackground": "var(--r-cl-23)",
+        "editor.lineHighlightBackground": "var(--r-cl-24)",
+        "editor.linkedEditingBackground": "var(--r-cl-23)",
+        "editor.selectionBackground": "var(--r-cl-25)",
+        "editor.selectionHighlightBackground": "var(--r-cl-26)",
+        "editor.selectionHighlightBorder": "var(--r-cl-27)",
+        "editor.stackFrameHighlightBackground": "var(--r-cl-28)",
+        "editor.wordHighlightBackground": "var(--r-cl-27)",
+        "editor.wordHighlightBorder": "var(--r-cl-29)",
+        "editor.wordHighlightStrongBackground": "var(--r-cl-27)",
+        "editor.wordHighlightStrongBorder": "var(--r-cl-30)",
+        "editorBracketHighlight.foreground1": "var(--r-cl-8)",
+        "editorBracketHighlight.foreground2": "var(--r-cl-31)",
+        "editorBracketHighlight.foreground3": "var(--r-cl-32)",
+        "editorBracketHighlight.foreground4": "var(--r-cl-8)",
+        "editorBracketHighlight.foreground5": "var(--r-cl-31)",
+        "editorBracketHighlight.foreground6": "var(--r-cl-32)",
+        "editorBracketMatch.background": "var(--r-cl-26)",
+        "editorBracketMatch.border": "var(--r-cl-27)",
+        "editorCursor.foreground": "var(--r-cl-33)",
+        "editorError.foreground": "var(--r-cl-34)",
+        "editorGroup.border": "var(--r-cl-3)",
+        "editorGroupHeader.tabsBackground": "var(--r-cl-24)",
+        "editorGroupHeader.tabsBorder": "var(--r-cl-3)",
+        "editorGutter.addedBackground": "var(--r-cl-35)",
+        "editorGutter.deletedBackground": "var(--r-cl-36)",
+        "editorGutter.modifiedBackground": "var(--r-cl-6)",
+        "editorIndentGuide.activeBackground": "var(--r-cl-37)",
+        "editorIndentGuide.background": "var(--r-cl-38)",
+        "editorLineNumber.activeForeground": "var(--r-cl-22)",
+        "editorLineNumber.foreground": "var(--r-cl-39)",
+        "editorOverviewRuler.border": "var(--r-cl-2)",
+        "editorWarning.foreground": "var(--r-cl-40)",
+        "editorWhitespace.foreground": "var(--r-cl-15)",
+        "editorWidget.background": "var(--r-cl-24)",
+        errorForeground: "var(--r-cl-34)",
+        focusBorder: "var(--r-cl-6)",
+        foreground: "var(--r-cl-41)",
+        "gitDecoration.addedResourceForeground": "var(--r-cl-35)",
+        "gitDecoration.conflictingResourceForeground": "var(--r-cl-31)",
+        "gitDecoration.deletedResourceForeground": "var(--r-cl-36)",
+        "gitDecoration.ignoredResourceForeground": "var(--r-cl-5)",
+        "gitDecoration.modifiedResourceForeground": "var(--r-cl-8)",
+        "gitDecoration.submoduleResourceForeground": "var(--r-cl-5)",
+        "gitDecoration.untrackedResourceForeground": "var(--r-cl-35)",
+        "input.background": "var(--r-cl-11)",
+        "input.border": "var(--r-cl-3)",
+        "input.foreground": "var(--r-cl-4)",
+        "input.placeholderForeground": "var(--r-cl-5)",
+        "list.activeSelectionBackground": "var(--r-cl-42)",
+        "list.activeSelectionForeground": "var(--r-cl-4)",
+        "list.focusBackground": "var(--r-cl-43)",
+        "list.hoverBackground": "var(--r-cl-44)",
+        "list.hoverForeground": "var(--r-cl-4)",
+        "list.inactiveFocusBackground": "var(--r-cl-7)",
+        "list.inactiveSelectionBackground": "var(--r-cl-45)",
+        "list.inactiveSelectionForeground": "var(--r-cl-4)",
+        "notificationCenterHeader.background": "var(--r-cl-3)",
+        "notificationCenterHeader.foreground": "var(--r-cl-10)",
+        "notifications.background": "var(--r-cl-11)",
+        "notifications.border": "var(--r-cl-3)",
+        "notifications.foreground": "var(--r-cl-4)",
+        "notificationsErrorIcon.foreground": "var(--r-cl-36)",
+        "notificationsInfoIcon.foreground": "var(--r-cl-8)",
+        "notificationsWarningIcon.foreground": "var(--r-cl-31)",
+        "panel.background": "var(--r-cl-24)",
+        "panel.border": "var(--r-cl-3)",
+        "panelInput.border": "var(--r-cl-3)",
+        "panelTitle.activeBorder": "var(--r-cl-1)",
+        "panelTitle.activeForeground": "var(--r-cl-4)",
+        "panelTitle.inactiveForeground": "var(--r-cl-10)",
+        "pickerGroup.border": "var(--r-cl-3)",
+        "pickerGroup.foreground": "var(--r-cl-4)",
+        "progressBar.background": "var(--r-cl-6)",
+        "quickInput.background": "var(--r-cl-11)",
+        "quickInput.foreground": "var(--r-cl-4)",
+        "scrollbar.shadow": "var(--r-cl-10)",
+        "scrollbarSlider.activeBackground": "var(--r-cl-47)",
+        "scrollbarSlider.background": "var(--r-cl-48)",
+        "scrollbarSlider.hoverBackground": "var(--r-cl-49)",
+        "settings.headerForeground": "var(--r-cl-4)",
+        "settings.modifiedItemIndicator": "var(--r-cl-6)",
+        "sideBar.background": "var(--r-cl-24)",
+        "sideBar.border": "var(--r-cl-3)",
+        "sideBar.foreground": "var(--r-cl-9)",
+        "sideBarSectionHeader.background": "var(--r-cl-24)",
+        "sideBarSectionHeader.border": "var(--r-cl-3)",
+        "sideBarSectionHeader.foreground": "var(--r-cl-4)",
+        "sideBarTitle.foreground": "var(--r-cl-4)",
+        "statusBar.background": "var(--r-cl-2)",
+        "statusBar.border": "var(--r-cl-3)",
+        "statusBar.debuggingBackground": "var(--r-cl-1)",
+        "statusBar.debuggingForeground": "var(--r-cl-2)",
+        "statusBar.foreground": "var(--r-cl-9)",
+        "statusBar.noFolderBackground": "var(--r-cl-2)",
+        "statusBarItem.prominentBackground": "var(--r-cl-45)",
+        "statusBarItem.remoteBackground": "var(--r-cl-2)",
+        "statusBarItem.remoteForeground": "var(--r-cl-9)",
+        "tab.activeBackground": "var(--r-cl-2)",
+        "tab.activeBorder": "var(--r-cl-2)",
+        "tab.activeBorderTop": "var(--r-cl-1)",
+        "tab.activeForeground": "var(--r-cl-4)",
+        "tab.border": "var(--r-cl-3)",
+        "tab.hoverBackground": "var(--r-cl-2)",
+        "tab.inactiveBackground": "var(--r-cl-24)",
+        "tab.inactiveForeground": "var(--r-cl-10)",
+        "tab.unfocusedActiveBorder": "var(--r-cl-2)",
+        "tab.unfocusedActiveBorderTop": "var(--r-cl-3)",
+        "tab.unfocusedHoverBackground": "var(--r-cl-2)",
+        "terminal.ansiBlack": "var(--r-cl-22)",
+        "terminal.ansiBlue": "var(--r-cl-50)",
+        "terminal.ansiBrightBlack": "var(--r-cl-5)",
+        "terminal.ansiBrightBlue": "var(--r-cl-8)",
+        "terminal.ansiBrightCyan": "var(--r-cl-51)",
+        "terminal.ansiBrightGreen": "var(--r-cl-52)",
+        "terminal.ansiBrightMagenta": "var(--r-cl-32)",
+        "terminal.ansiBrightRed": "var(--r-cl-34)",
+        "terminal.ansiBrightWhite": "var(--r-cl-15)",
+        "terminal.ansiBrightYellow": "var(--r-cl-53)",
+        "terminal.ansiCyan": "var(--r-cl-54)",
+        "terminal.ansiGreen": "var(--r-cl-35)",
+        "terminal.ansiMagenta": "var(--r-cl-32)",
+        "terminal.ansiRed": "var(--r-cl-36)",
+        "terminal.ansiWhite": "var(--r-cl-10)",
+        "terminal.ansiYellow": "var(--r-cl-55)",
+        "terminal.foreground": "var(--r-cl-9)",
+        "terminal.tab.activeBorder": "var(--r-cl-1)",
+        "terminalCursor.background": "var(--r-cl-15)",
+        "terminalCursor.foreground": "var(--r-cl-8)",
+        "textBlockQuote.background": "var(--r-cl-11)",
+        "textBlockQuote.border": "var(--r-cl-3)",
+        "textCodeBlock.background": "var(--r-cl-24)",
+        "textLink.activeForeground": "var(--r-cl-8)",
+        "textLink.foreground": "var(--r-cl-50)",
+        "textPreformat.foreground": "var(--r-cl-9)",
+        "textSeparator.foreground": "var(--r-cl-15)",
+        "titleBar.activeBackground": "var(--r-cl-2)",
+        "titleBar.activeForeground": "var(--r-cl-4)",
+        "titleBar.border": "var(--r-cl-3)",
+        "titleBar.inactiveBackground": "var(--r-cl-24)",
+        "titleBar.inactiveForeground": "var(--r-cl-10)",
+        "tree.indentGuidesStroke": "var(--r-cl-3)",
+        "welcomePage.buttonBackground": "var(--r-cl-24)",
+        "welcomePage.buttonHoverBackground": "var(--r-cl-3)",
+    },
+    displayName: "GitHub Dynamic",
+    name: "github-dynamic",
+    semanticHighlighting: true,
+    tokenColors: [
+        {
+            scope: ["comment", "punctuation.definition.comment", "string.comment"],
+            settings: {
+                foreground: "var(--r-cl-10)",
+            },
+        },
+        {
+            scope: [
+                "constant",
+                "entity.name.constant",
+                "variable.other.constant",
+                "variable.other.enummember",
+                "variable.language",
+            ],
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: ["entity", "entity.name"],
+            settings: {
+                foreground: "var(--r-cl-56)",
+            },
+        },
+        {
+            scope: "variable.parameter.function",
+            settings: {
+                foreground: "var(--r-cl-22)",
+            },
+        },
+        {
+            scope: "entity.name.tag",
+            settings: {
+                foreground: "var(--r-cl-52)",
+            },
+        },
+        {
+            scope: "keyword",
+            settings: {
+                foreground: "var(--r-cl-36)",
+            },
+        },
+        {
+            scope: ["storage", "storage.type"],
+            settings: {
+                foreground: "var(--r-cl-36)",
+            },
+        },
+        {
+            scope: ["storage.modifier.package", "storage.modifier.import", "storage.type.java"],
+            settings: {
+                foreground: "var(--r-cl-22)",
+            },
+        },
+        {
+            scope: ["string", "punctuation.definition.string", "string punctuation.section.embedded source"],
+            settings: {
+                foreground: "var(--r-cl-58)",
+            },
+        },
+        {
+            scope: "support",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "meta.property-name",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "variable",
+            settings: {
+                foreground: "var(--r-cl-31)",
+            },
+        },
+        {
+            scope: "variable.other",
+            settings: {
+                foreground: "var(--r-cl-22)",
+            },
+        },
+        {
+            scope: "invalid.broken",
+            settings: {
+                fontStyle: "italic",
+                foreground: "var(--r-cl-59)",
+            },
+        },
+        {
+            scope: "invalid.deprecated",
+            settings: {
+                fontStyle: "italic",
+                foreground: "var(--r-cl-59)",
+            },
+        },
+        {
+            scope: "invalid.illegal",
+            settings: {
+                fontStyle: "italic",
+                foreground: "var(--r-cl-59)",
+            },
+        },
+        {
+            scope: "invalid.unimplemented",
+            settings: {
+                fontStyle: "italic",
+                foreground: "var(--r-cl-59)",
+            },
+        },
+        {
+            scope: "carriage-return",
+            settings: {
+                background: "var(--r-cl-36)",
+                fontStyle: "italic underline",
+                foreground: "var(--r-cl-11)",
+            },
+        },
+        {
+            scope: "message.error",
+            settings: {
+                foreground: "var(--r-cl-59)",
+            },
+        },
+        {
+            scope: "string variable",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: ["source.regexp", "string.regexp"],
+            settings: {
+                foreground: "var(--r-cl-58)",
+            },
+        },
+        {
+            scope: [
+                "string.regexp.character-class",
+                "string.regexp constant.character.escape",
+                "string.regexp source.ruby.embedded",
+                "string.regexp string.regexp.arbitrary-repitition",
+            ],
+            settings: {
+                foreground: "var(--r-cl-58)",
+            },
+        },
+        {
+            scope: "string.regexp constant.character.escape",
+            settings: {
+                fontStyle: "bold",
+                foreground: "var(--r-cl-52)",
+            },
+        },
+        {
+            scope: "support.constant",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "support.variable",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "meta.module-reference",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "punctuation.definition.list.begin.markdown",
+            settings: {
+                foreground: "var(--r-cl-31)",
+            },
+        },
+        {
+            scope: ["markup.heading", "markup.heading entity.name"],
+            settings: {
+                fontStyle: "bold",
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "markup.quote",
+            settings: {
+                foreground: "var(--r-cl-52)",
+            },
+        },
+        {
+            scope: "markup.italic",
+            settings: {
+                fontStyle: "italic",
+                foreground: "var(--r-cl-22)",
+            },
+        },
+        {
+            scope: "markup.bold",
+            settings: {
+                fontStyle: "bold",
+                foreground: "var(--r-cl-22)",
+            },
+        },
+        {
+            scope: ["markup.underline"],
+            settings: {
+                fontStyle: "underline",
+            },
+        },
+        {
+            scope: ["markup.strikethrough"],
+            settings: {
+                fontStyle: "strikethrough",
+            },
+        },
+        {
+            scope: "markup.inline.raw",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: ["markup.deleted", "meta.diff.header.from-file", "punctuation.definition.deleted"],
+            settings: {
+                background: "var(--r-cl-60)",
+                foreground: "var(--r-cl-59)",
+            },
+        },
+        {
+            scope: ["markup.inserted", "meta.diff.header.to-file", "punctuation.definition.inserted"],
+            settings: {
+                background: "var(--r-cl-61)",
+                foreground: "var(--r-cl-52)",
+            },
+        },
+        {
+            scope: ["markup.changed", "punctuation.definition.changed"],
+            settings: {
+                background: "var(--r-cl-62)",
+                foreground: "var(--r-cl-31)",
+            },
+        },
+        {
+            scope: ["markup.ignored", "markup.untracked"],
+            settings: {
+                background: "var(--r-cl-8)",
+                foreground: "var(--r-cl-24)",
+            },
+        },
+        {
+            scope: "meta.diff.range",
+            settings: {
+                fontStyle: "bold",
+                foreground: "var(--r-cl-56)",
+            },
+        },
+        {
+            scope: "meta.diff.header",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "meta.separator",
+            settings: {
+                fontStyle: "bold",
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: "meta.output",
+            settings: {
+                foreground: "var(--r-cl-8)",
+            },
+        },
+        {
+            scope: [
+                "brackethighlighter.tag",
+                "brackethighlighter.curly",
+                "brackethighlighter.round",
+                "brackethighlighter.square",
+                "brackethighlighter.angle",
+                "brackethighlighter.quote",
+            ],
+            settings: {
+                foreground: "var(--r-cl-9)",
+            },
+        },
+        {
+            scope: "brackethighlighter.unmatched",
+            settings: {
+                foreground: "var(--r-cl-57)",
+            },
+        },
+        {
+            scope: ["constant.other.reference.link", "string.other.link"],
+            settings: {
+                fontStyle: "underline",
+                foreground: "var(--r-cl-58)",
+            },
+        },
+    ],
+    type: "light",
+};
