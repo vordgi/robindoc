680e8ca0d74af91eefe2f701d7be747ef718cca0
Vordgi <sasha2822222@gmail.com>
2024-08-03 20:10:27 +0400

    rd-32 support sidebar drop lists

--- packages/robindoc/src/components/code.tsx
+++ packages/robindoc/src/components/code.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { cache } from "react";
 import { createHighlighter, type BuiltinLanguage } from "shiki";
 import { githubDynamic } from "../data/code-theme";
 
@@ -8,8 +8,27 @@ interface ShikiProps {
     className?: string;
 }
 
+const initBaseHighlighter = async () => {
+    const highlighter = await createHighlighter({ langs: [], themes: [] });
+    await highlighter.loadTheme(githubDynamic);
+    return highlighter;
+};
+
+const highlighterPromise = initBaseHighlighter();
+
+const getHighlighter = cache(async (language: BuiltinLanguage) => {
+    const highlighter = await highlighterPromise;
+    const loadedLanguages = highlighter.getLoadedLanguages();
+
+    if (!loadedLanguages.includes(language)) {
+        await highlighter.loadLanguage(language);
+    }
+
+    return highlighter;
+});
+
 export async function Shiki({ code, lang, className }: ShikiProps) {
-    const highlighter = await createHighlighter({ langs: [lang], themes: [] });
+    const highlighter = await getHighlighter(lang);
     await highlighter.loadTheme(githubDynamic);
 
     const html = highlighter.codeToHtml(code, {
