b8ce74786994218ebfa13430bc2e4a9490c855a0
Vordgi <sasha2822222@gmail.com>
2024-08-11 14:42:52 +0400

    rd-29 configure rollup build

---
+++ packages/robindoc/rollup.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const scss = require("rollup-plugin-scss");
+const { terser } = require("rollup-plugin-terser");
+const { default: preserveDirectives } = require("rollup-preserve-directives");
+
+module.exports = {
+    input: ["src/index.tsx", "src/assets/index.ts"],
+    output: [
+        {
+            dir: "lib",
+            format: "es",
+            sourcemap: true,
+            preserveModules: true,
+            assetFileNames: "[name][extname]",
+        },
+    ],
+    external: [
+        "react",
+        "gray-matter",
+        "marked",
+        "dot-prop",
+        "github-slugger",
+        "path",
+        "shiki",
+        "fs",
+        "fs/promises",
+        "glob",
+    ],
+    plugins: [
+        commonjs(),
+        typescript(),
+        scss({
+            outputStyle: "compressed",
+            output: true,
+            failOnError: true,
+            fileName: "styles.css",
+            sourceMap: true,
+            exclude: ["node_modules/"],
+        }),
+        terser(),
+        preserveDirectives(),
+    ],
+};
