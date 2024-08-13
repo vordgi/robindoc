94c33db0312c4d14d5953cf4fc643b48f8b53730
Vordgi <sasha2822222@gmail.com>
2024-08-13 22:05:51 +0400

    rd-90 animate contents and remove client js

--- packages/robindoc/src/components/contents/index.tsx
+++ packages/robindoc/src/components/contents/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "./contents.scss";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { CurrentSectionContext } from "../anchor-provider";
 import { detectGitType } from "../../utils/git-data";
 
@@ -12,46 +12,42 @@ export interface ContentsProps extends React.PropsWithChildren {
 }
 
 export const Contents: React.FC<ContentsProps> = ({ headings, hideContents, editOnGitUri }) => {
-    const [opened, setOpened] = useState(false);
     const currentSection = useContext(CurrentSectionContext);
-
-    const toggleHandler = () => {
-        if (window.innerWidth < 1080) {
-            setOpened(!opened);
-        }
-    };
 
     return (
         <div className="r-contents">
-            <div className={`r-contents-sticky${opened ? " _opened" : ""}`}>
+            <div className="r-contents-sticky">
                 {headings.length > 0 && !hideContents && (
                     <>
-                        <div className="r-contents-title" onClick={toggleHandler}>
-                            On this page
-                            <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="r-contents-chevron"
-                            >
-                                <path d="m6 9 6 6 6-6" />
-                            </svg>
-                        </div>
-                        <div className="r-contents-list">
-                            {headings.map((heading, index) => (
-                                <a
-                                    href={`#${heading.id}`}
-                                    key={heading.id}
-                                    className={`r-contents-link${heading.nested ? " _nested" : ""}${currentSection !== null && index <= currentSection ? " _passed" : ""}${currentSection === index ? " _active" : ""}`}
+                        <input type="checkbox" className="r-contents-control" id="r-contents" />
+                        <div className="r-contents-details">
+                            <label className="r-contents-title" htmlFor="r-contents">
+                                On this page
+                                <svg
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="r-contents-chevron"
                                 >
-                                    {heading.title}
-                                </a>
-                            ))}
+                                    <path d="m6 9 6 6 6-6" />
+                                </svg>
+                            </label>
+                            <div className="r-contents-list">
+                                {headings.map((heading, index) => (
+                                    <a
+                                        href={`#${heading.id}`}
+                                        key={heading.id}
+                                        className={`r-contents-link${heading.nested ? " _nested" : ""}${currentSection !== null && index <= currentSection ? " _passed" : ""}${currentSection === index ? " _active" : ""}`}
+                                    >
+                                        {heading.title}
+                                    </a>
+                                ))}
+                            </div>
                         </div>
                     </>
                 )}
