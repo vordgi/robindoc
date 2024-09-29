a598c17fd535ea2778cb95f19b11400496e862c4
Vordgi <sasha2822222@gmail.com>
2024-09-29 16:10:50 +0400

    rd-234 support extra items in structure

--- packages/robindoc/src/core/types/structure.ts
+++ packages/robindoc/src/core/types/structure.ts
@@ -1,13 +1,16 @@
 import { type Configuration } from "./content";
 
-export type DocItem = {
-    type?: "heading" | "row";
-    title: string;
-    hidden?: boolean;
-    items?: DocItem[] | null | "auto" | "auto-spreaded";
-    href?: string;
-    configuration?: Configuration;
-};
+export type DocItem =
+    | {
+          type?: "heading" | "row";
+          title: string;
+          hidden?: boolean;
+          items?: DocItem[] | null;
+          href?: string;
+          configuration?: Configuration;
+      }
+    | "auto"
+    | "auto-spreaded";
 
 export type Structure = {
     configuration?: Configuration;
