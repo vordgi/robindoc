865437149b9a9060a30f3668bdc6c5123e72ff91
Vordgi <sasha2822222@gmail.com>
2024-10-13 14:42:53 +0400

    rd-297 improve sidebar generation

--- packages/robindoc/src/components/elements/sidebar/tools.ts
+++ packages/robindoc/src/components/elements/sidebar/tools.ts
@@ -1,9 +1,11 @@
 import { type TreeItem } from "./types";
 
 export const checkIsTargetSection = (item: TreeItem, pathname?: string) => {
-    if (!pathname) return false;
+    if (typeof pathname !== "string") return false;
 
-    if (item.href && pathname === new URL(item.href, "http://r").pathname?.replace(/\/$/, "")) return true;
+    if (typeof item.href === "string" && pathname === new URL(item.href, "http://r").pathname?.replace(/\/$/, "")) {
+        return true;
+    }
 
     if (item.items?.find((el) => checkIsTargetSection(el, pathname))) return true;
 
@@ -11,7 +13,7 @@ export const checkIsTargetSection = (item: TreeItem, pathname?: string) => {
 };
 
 export const checkIsTargetPathname = (itemHref?: string, pathname?: string) => {
-    if (!pathname || !itemHref) return false;
+    if (typeof pathname !== "string" || typeof itemHref !== "string") return false;
 
     return pathname === new URL(itemHref, "http://r").pathname?.replace(/\/$/, "");
 };
