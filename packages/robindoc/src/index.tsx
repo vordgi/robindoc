293d089e17e8d458c2c87f4ba6ad68cda35ff114
Vordgi <sasha2822222@gmail.com>
2024-10-16 20:04:54 +0400

    rd-326 implement page container

--- packages/robindoc/src/index.tsx
+++ packages/robindoc/src/index.tsx
@@ -4,5 +4,6 @@ export { Footer } from "./components/elements/footer";
 export { RobinProvider } from "./components/elements/robin-provider";
 export { DocsContainer } from "./components/elements/docs-container";
 export { BlogContainer } from "./components/elements/blog-container";
+export { PageContainer } from "./components/elements/page-container";
 export { KeylinkToContent } from "./components/elements/keylink-to-content";
 export { KeylinkToNavigation } from "./components/elements/keylink-to-navigation";
